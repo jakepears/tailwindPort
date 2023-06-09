ccb3ced86f4c2a546bd2d72226f8c4a0f8e26416
DaCodeWiz <jake.pearson.1694@gmail.com>
2023-06-08 17:41:54 -0700

    error msg

---
+++ src/app/about/error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function error() {
+  return (
+    <div>
+        <h1>404</h1>
+        <h3>I honestly don't know why the link didn't work. Either you messed up, or I did. I'll look into it though :P</h3>
+    </div>
+  )
+}
