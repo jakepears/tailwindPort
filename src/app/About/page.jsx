2834cb84579497f4e307f859999b7fd48b7219d9
DaCodeWiz <jake.pearson.1694@gmail.com>
2023-06-08 19:23:02 -0700

    analytics fix

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,6 +1,12 @@
 
 import AboutPage from "@/Components/About/AboutPage.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function about() {
-  return <AboutPage /> 
+  return(
+    <>
+    <AboutPage />
+    <Analytics />
+    </>
+  )
 }
