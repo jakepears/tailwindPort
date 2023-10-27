2d382b9fdddab6a8fe22e0045957dd85b8db284f
DaCodeWiz <jake.pearson.1694@gmail.com>
2023-10-27 13:59:11 -0700

    changes

--- src/Components/Loader/PreloaderContext.jsx
+++ src/Components/Loader/PreloaderContext.jsx
@@ -1,20 +1,37 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PreloaderUI from "./PreloaderUI";
+import gsap from "gsap";
 
 export default function PreloaderContext({ children }) {
   const [loading, setLoading] = useState(true);
   const [progress, setProgress] = useState(0);
+  const loaderRef = useRef(null);
 
   useEffect(() => {
-    if (progress === 99) {
-      setLoading(false);
+    const animation = () => {
+      gsap.to(loaderRef.current, {
+        opacity: 0,
+        duration: 1.5,
+        delay: 0.95,
+        onComplete: () => setLoading(false),
+      });
+    };
+    if (progress === 50) {
+      animation();
     }
+    requestAnimationFrame(animation);
   }, [progress]);
+
   return (
     <>
       {loading && (
-        <PreloaderUI updateLoadingProgress={setProgress} progress={progress} />
+        <div ref={loaderRef}>
+          <PreloaderUI
+            updateLoadingProgress={setProgress}
+            progress={progress}
+          />
+        </div>
       )}
       {children}
     </>
